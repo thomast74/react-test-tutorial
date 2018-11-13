fd93eede4de3cfaeea88cea56e481c667c1905ab
Thomas Trageser <thomast74@gmail.com>
2018-11-13 22:26:55 +0800

    Test application ready

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+export const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1
+});
+
+export const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1
+});
+
+
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      counter: 0,
+    };
+
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+
   render() {
+    const { counter } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>My Counter</h1>
+        <Counter counter={counter} />
+
+        <button
+          type="button"
+          onClick={this.onIncrement}
+        >
+          Increment
+        </button>
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
       </div>
     );
   }
 }
+
+
+export const Counter = ({ counter }) => 
+  <p>{counter}</p>
+
 
 export default App;
